dff6f4a7553d123ba92300e0d95d899ec8b96f92
Davyd McColl <davydm@gmail.com>
2018-11-22 20:14:12 +0200

    test: prove that extra commas don't count

--- tests/args.spec.js
+++ tests/args.spec.js
@@ -117,6 +117,27 @@ describe("args", () => {
         expect(result.vars[0]).toEqual(expected);
       });
 
+      it(`should not be confused by extra commas`, async() => {
+        // Arrange
+        const
+          args = ["-moo", ",-0.1,", "0.2,", "0.3,", "0.4"],
+          expected = {
+            name: "moo",
+            modifiers: [
+              { op: "-", value: 0.1 },
+              { op: "-", value: 0.2 },
+              { op: "-", value: 0.3 },
+              { op: "-", value: 0.4 }]
+          };
+        // Act
+        const result = await parse(args);
+        // Assert
+        expect(result).toExist();
+        expect(result.vars).toExist();
+        expect(result.vars).toHaveLength(1);
+        expect(result.vars[0]).toEqual(expected);
+      });
+
     });
   });
 
