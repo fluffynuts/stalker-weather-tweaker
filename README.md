ff7f604307ae5de828b306c07abb92d31bd7ccb2
Davyd McColl <davydm@gmail.com>
2018-11-25 21:35:25 +0200

    feat: first official, downloadable release

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # weather-tweaker
 Weather variables tweaker for S.T.A.L.K.E.R weather files
+
+
+# Usage:
+
+## Simplest 
+1. Install NodeJS on your machine (at least 8.12)
+2. `npm install -g stalker-weather-tweaker`
+3. `stalker-weather-tweaker --help`
+
+## If you have node and this source, do:
+1. `npm install`
+2. `npm run tweak -- --help` to find out how to use the tool
+3. `npm run tweak -- ...args` to run the tool as desired
+
+## Building stand-alone versions using `pkg`
+```
+npm run build-tweak
+```
+The output files are rather large since they embed node, but it means you don't
+need node at the computer where you want to use these tools.
--- src/cli/tweak.js
+++ src/cli/tweak.js
@@ -1,3 +1,32 @@
+#!/usr/bin/env node
+function showHelp() {
+  [
+    "S.T.A.L.K.E.R weather variable tweaker",
+    "  Usage:",
+    "  tweak [file] {..file} {options}",
+    "    where options are of the form:",
+    "    -variable (+|-|=|~)value",
+    "",
+    "    variables may be:",
+    "      increased: +",
+    "      decreased: -",
+    "      set      : =",
+    "      unchanged: ~",
+    "",
+    "  example:",
+    "",
+    "    tweak file1.ltx file2.ltx -fog_color +0.01 -0.02 =0.04",
+    "",
+    "    would modify files file1.ltx and file2.ltx,",
+    "      - modifying all found fog_color variables:",
+    "      - adding 0.01 to the first parameter",
+    "      - subtracting 0.02 from the second parameter",
+    "      - setting the third parameter to 0.04",
+    "  hints:",
+    "    extra parameters or settings are ignored",
+    "    use ~ to 'skip over' a parameter you don't want to change"
+  ].forEach(line => console.log(line));
+}
 (async function () {
   const
     path = require("path"),
@@ -15,6 +44,12 @@
       }
       return acc;
     }, []);
+
+  if (args.length === 0 ||
+    args.indexOf("-h") > -1 ||
+    args.indexOf("--help") > -1) {
+    return showHelp();
+  }
 
   await tweak(args);
 })();
